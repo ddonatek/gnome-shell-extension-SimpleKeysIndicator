d8a1878a71d44327d8a6234d38a9e5b4a1f881d2
David Donátek <david.donatek@gmail.com>
2019-10-15 23:38:25 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+## simple-keys-indicator
+GNOME Shell extension that displays <kbd>CapsLock</kbd> and <kbd>NumLock</kbd> status
+
+![Screenshot](./screenshot.png)
+
+## Install
+Download and unzip to ```~/.local/share/gnome-shell/extensions```
+
+## Why?
+Less code means smaller attack vector for the GNOME Shell devs
+
+Inspired by (at the time of writing broken) [keys-indicator@caasiu.github.com](https://github.com/caasiu/gnome-shell-extension-KeysIndicator)
---
+++ simple-keys-indicator@ddonatek.github.com/extension.js
@@ -0,0 +1,45 @@
+
+const St = imports.gi.St;
+const Main = imports.ui.main;
+const Gdk = imports.gi.Gdk;
+
+const Keymap = Gdk.Keymap.get_default();
+
+let numLock, capsLock;
+
+function keyStatusChanged() {
+	// TODO add option to hide when not active? (numLock|capsLock).visible = true|false;
+	if (Keymap.get_num_lock_state()) {
+		numLock.remove_style_class_name("status-off");
+	} else {
+		numLock.add_style_class_name("status-off");
+	}
+	if (Keymap.get_caps_lock_state()) {
+		capsLock.remove_style_class_name("status-off");
+	} else {
+		capsLock.add_style_class_name("status-off");
+	}
+}
+
+function init() {
+	numLock = new St.Label({
+		style_class: "status-label",
+		text: "1"
+	});
+	capsLock = new St.Label({
+		style_class: "status-label",
+		text: "A"
+	});
+	Keymap.connect("state_changed", keyStatusChanged);
+}
+
+function enable() {
+	Main.panel._rightBox.insert_child_at_index(capsLock, 0);
+	Main.panel._rightBox.insert_child_at_index(numLock, 0);
+	keyStatusChanged();
+}
+
+function disable() {
+	Main.panel._rightBox.remove_child(numLock, 0);
+	Main.panel._rightBox.remove_child(capsLock, 0);
+}
